fba7cc6ae79a94a36ad78b580bc2497bc17c050c
Isaac A. Marin <VasMar_ia@hotmail.com>
2020-01-13 12:12:02 -0600

    first commit

---
+++ funcion.py
@@ -0,0 +1,18 @@
+from random import randint
+
+funct_y=[]
+funct_x=[]
+param_ab=[]
+param_ab_bin=[]
+
+def seleccion():
+    bin
+
+def int_to_bin(value):
+    binary="{0:06b}".format(value)
+    return [int(x) for x in str(binary)]
+
+def _generate_params():
+    num = randint(1,59)
+
+seleccion()
