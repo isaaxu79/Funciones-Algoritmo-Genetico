e8d66c506aa153f4e6ed223ff16981818eea4046
Isaac A. Marin <VasMar_ia@hotmail.com>
2020-01-16 18:31:10 -0600

    saved

--- funcion.py
+++ funcion.py
@@ -3,18 +3,47 @@ import math
 import random
 
 funct_y=[0.0798,0.7741,0.1221,0.4388,1.0291,0.1070,-0.1179,0.3997,-0.4426,-0.7845,0.0568,-0.2774,-0.5127,0.5686,0.5437,0.0548,0.8502,0.7436,-0.2100,0.1904,0.1803]
-funct_x=[0.0000,0.500,0.5000,0.7500,1.0000,1.2500,1.5000,1.7500,2.0000,2.2500,2.5000,2.7500,3.0000,3.2500,3.5000,3.7500,4.0000,4.2500,4.5000,4.7500,5.0000]
+funct_x=[0.0000,0.2500,0.5000,0.7500,1.0000,1.2500,1.5000,1.7500,2.0000,2.2500,2.5000,2.7500,3.0000,3.2500,3.5000,3.7500,4.0000,4.2500,4.5000,4.7500,5.0000]
 param_ab=[]
-param_ab_bin=[]
+minu=[]
 res_func=[] #Guarda los resultados de la funcion
-res_fit=[] #Guarda el fit de cada uno en una lista
-best = [] #Guarda los 5 mejores individuos. 
+res_fit=[] #Guarda el fit
+bestfit = []
+
+
 POBLACION=10 
 res_sum=0 #La suma de todas las respuestas de la funcion
 
 
+#
+# [1,3],[23,59],[2,5],[]
+#
 def selection(params_gen):
-    pass
+    #primero sacamos el resultado de la funcion y despues el fitness
+    generation_function=[]
+    for prob in params_gen:  #[2,2]
+        aux=[]
+        for z in funct_x: #x desma
+            var = fitProbability(z,prob[0],prob[1])
+            aux.append(round(var,4))
+        fit = fitnessCal(aux)
+        fit = abs(fit)
+        generation_function.append([aux,fit,prob])
+    # siguiente
+    for x in generation_function:
+        print()
+    return generation_function
+
+def fitnessCal(datas):
+    valor=0
+    for dato in datas:
+        valor +=dato
+    return valor/len(datas)
+
+def fitProbability(num,a,b):
+    convert_radians_a=math.radians(a*num*0.1)
+    convert_radians_b = math.radians(b*num*0.1)
+    return float(math.cos(a*num*0.1)*math.sin(b*num*0.1))
 
 def bin_to_int(num_bin):
     letra=""
@@ -42,21 +71,24 @@ def crossover(datas):
         aux.extend(aux_impar[x])
         datos.append(aux)#y0a1|y1a0
     return datos
+
+def mutation():
+    aux = []
+    index = 0
+    for x in datos:
+        ans = bool(random.getrandbits(1))
+        if ans:
+            for y in range(len(x)):
+                if bool(random.getrandbits(1)):
+                    if datos[index][y] == 1:
+                        datos[index][y] = 0
+                    else:
+                        datos[index][y] = 1
+        index+=1
             
 
-def searchInfo():
-    ObtenerPoblacion() #Se obtienen los resultados y se agregan a una lista. 
-    getFit() #Se genera una lista con el fit de cada uno de los resultados. 
-
-def getMenor():
     pass
     #for i in res_fit:
-           
-def getFit():
-    for i in res_func:
-        e = i / res_sum
-        res_fit.append(e) #Aqui se agrega el fit de cada uno a una lista
-    print(res_fit)
 
 def ObtenerPoblacion():
     global res_sum
@@ -69,32 +101,20 @@ def ObtenerPoblacion():
         res_func.append(funcionEjecutada(x,a,b)) #Guarda los resultados de la funcion en una lista
     res_sum=sum(res_func) #Suma de todos los resultados. 
    
-## Esta función recibe los parametros para ejecutar los resultados.         
-def funcionEjecutada(num,a,b):
-    resultado = float(math.cos(a*num)*math.sin(b*num))
-    return resultado
-
-#
-# Transforma los valores enteros a binarios dentro de un arreglo
-#
 def int_to_bin(value):
     binary="{0:06b}".format(value)
     return [int(x) for x in str(binary)]
-#
-# Crea los parametros del rango 1-59
-#
+
 def _generate_params():
     aux=[]
     aux.append(randint(1,59))
     aux.append(randint(1,59))
     param_ab.append(aux)
-    #param_ab_bin.append(int_to_bin(param_ab[0]))
-    #param_ab_bin.append(int_to_bin(param_ab[1]))
 
 if __name__ == "__main__":
-    _generate_params()
-    _generate_params()
-    print(param_ab)
+    for _ in range(4):
+        _generate_params()
+    auc = selection(param_ab)
     #print(param_ab_bin)
     # Iniciamos el proceso de seleccion
     #seleccion(array)
